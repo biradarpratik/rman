49d1ceb4f425a1335b34df5b5edde79aa8379fcc
Pratik Biradar <84512811+biradarpratik@users.noreply.github.com>
2023-05-25 12:51:23 +0530

    Update ChatFrameExample.java

--- ChatFrameExample.java
+++ ChatFrameExample.java
@@ -1,56 +1,73 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatFrameExample {
     public static void main(String[] args) {
-        // Create the main frame
         JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
-        frame.setLayout(null);
 
-        // Create the menu bar
+        // Create panel for components
+        JPanel panel = new JPanel(new BorderLayout());
+
+        // Create menu bar
         JMenuBar menuBar = new JMenuBar();
-
-        // Create the "File" menu
         JMenu fileMenu = new JMenu("File");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveAsItem = new JMenuItem("Save As");
+        fileMenu.add(openItem);
+        fileMenu.add(saveAsItem);
         menuBar.add(fileMenu);
-
-        // Create the "Open" menu item
-        JMenuItem openMenuItem = new JMenuItem("OPEN");
-        fileMenu.add(openMenuItem);
-
-        // Create the "Save As" menu item
-        JMenuItem saveAsMenuItem = new JMenuItem("SAVE AS");
-        fileMenu.add(saveAsMenuItem);
-
-        // Create the "Help" menu
         JMenu helpMenu = new JMenu("Help");
         menuBar.add(helpMenu);
-
-        // Add the menu bar to the frame
         frame.setJMenuBar(menuBar);
 
-        // Create the "ENTER TEXT" label
+        // Create label
         JLabel enterTextLabel = new JLabel("ENTER TEXT:");
-        enterTextLabel.setBounds(20, 30, 80, 20);
-        frame.add(enterTextLabel);
 
-        // Create the text field
-        JTextField textField = new JTextField();
-        textField.setBounds(100, 30, 200, 20);
-        frame.add(textField);
+        // Create text field
+        JTextField textField = new JTextField(20);
 
-        // Create the "SEND" button
+        // Create send button
         JButton sendButton = new JButton("SEND");
-        sendButton.setBounds(100, 70, 80, 30);
-        frame.add(sendButton);
 
-        // Create the "RESET" button
+        // Create reset button
         JButton resetButton = new JButton("RESET");
-        resetButton.setBounds(200, 70, 80, 30);
-        frame.add(resetButton);
 
-        // Make the frame visible
+        // Create panel for label, text field, send button, and reset button
+        JPanel inputPanel = new JPanel(new FlowLayout());
+        inputPanel.add(enterTextLabel);
+        inputPanel.add(textField);
+        inputPanel.add(sendButton);
+        inputPanel.add(resetButton);
+
+        // Add input panel to the main panel
+        panel.add(inputPanel, BorderLayout.SOUTH);
+
+        // Add action listener for send button
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = textField.getText();
+                System.out.println("Sending text: " + text);
+                textField.setText("");
+            }
+        });
+
+        // Add action listener for reset button
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("");
+            }
+        });
+
+        // Add panel to frame
+        frame.getContentPane().add(panel);
+
+        // Display the frame
         frame.setVisible(true);
     }
 }
